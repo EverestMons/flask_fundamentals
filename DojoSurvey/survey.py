332089bab08a631ca1a91495d2de4f87e970d5fd
Mark Lehn <marklehn@icloud.com>
2017-03-09 19:00:11 -0600

    Uploading ninja_gold

--- DojoSurvey/survey.py
+++ DojoSurvey/survey.py
@@ -1,18 +1,22 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
-
+app.secret_key = 'thekey'
 @app.route('/')
-
 def index():
-    return render_template("index.php")
-
-
-@app.route('/result',methods=["POST"])
-def result():
-    return render_template("result.html", name = request.form['name'] ,
-    com = request.form['comment'] ,
-    location = request.form['location'] ,
-    favlang = request.form['favlang'])
+    return render_template("index.html")
+@app.route('/process', methods=["POST"])
+def process():
+    if len(request.form["name"]) < 1:
+        flash("Please give your name")
+        return redirect('/')
+    if len(request.form["comment"]) < 1:
+        flash("Please make a comment")
+        return redirect('/')
+    if len(request.form["comment"]) > 120:
+        flash("Comment must be under 120 characters")
+        return redirect('/')
+    else:
+        flash("Success")
     return redirect('/')
 app.run(debug=True)
